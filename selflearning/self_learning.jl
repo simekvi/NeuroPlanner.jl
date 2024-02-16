48d958725cdcbd527051ece36b2f04463b1942fd
Vítězslav Šimek <simekvi1@fel.cvut.cz>
2024-02-16 12:37:46 +0100

    self_learning back to original

--- selflearning/self_learning.jl
+++ selflearning/self_learning.jl
@@ -24,65 +24,65 @@ include("training.jl")
 include("utils.jl")
 
 function experiment(domain_pddl, hnet, problem_files, fminibatch, planner; max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, max_epochs, max_sim_time, filename)
-    isempty(filename) || isdir(dirname(filename)) || mkpath(dirname(filename))
-    opt_type = Symbol(opt_type)
-    domain = load_domain(domain_pddl)
-    pddld = hnet(domain; message_passes=graph_layers, residual)
-    solutions = Vector{Any}(fill(nothing, length(problem_files)))
+	isempty(filename) || isdir(dirname(filename)) || mkpath(dirname(filename))
+	opt_type = Symbol(opt_type)
+	domain = load_domain(domain_pddl)
+	pddld = hnet(domain; message_passes = graph_layers, residual)
+	solutions = Vector{Any}(fill(nothing, length(problem_files)))
 
-    #create model from some problem instance
-    model = let
-        problem = load_problem(first(problem_files))
-        pddle, state = initproblem(pddld, problem)
-        h₀ = pddle(state)
-        model = reflectinmodel(h₀, d -> ffnn(d, dense_dim, dense_dim, dense_layers); fsm=Dict("" => d -> ffnn(d, dense_dim, 1, dense_layers)))
-    end
+	#create model from some problem instance
+	model = let 
+		problem = load_problem(first(problem_files))
+		pddle, state = initproblem(pddld, problem)
+		h₀ = pddle(state)
+		model = reflectinmodel(h₀, d -> ffnn(d, dense_dim, dense_dim, dense_layers);fsm = Dict("" =>  d -> ffnn(d, dense_dim, 1, dense_layers)))
+	end
 
-    offset = 1
-    # opt = Flux.Optimise.Optimiser(AdaBelief(), WeightDecay(1f-4));
-    opt = AdaBelief()
-    ps = Flux.params(model)
-    all_solutions = []
-    losses = []
-    fvals = fill(typemax(Float64), length(solutions))
-    start_time = time()
-    logger = tblogger(filename * "_events.pb")
-    for epoch in 1:max_epochs
-        solved_before = issolved.(solutions)
-        offset = 1
-        while offset < length(solutions)
-            log_value(logger, "offset", offset)
-            offset, updated = update_solutions!(solutions, pddld, model, problem_files, dedup_fmb ∘ fminibatch, planner; offset, solve_solved, stop_after, max_time, artificial_goals)
-            # print some Statistics
-            solved = findall(issolved.(solutions))
-            print("offset = ", offset, " updated = ", length(updated), " ")
-            log_value(logger, "solved", length(solved))
-            show_stats(solutions)
-            length(solved) == length(solutions) && break
+	offset = 1
+	# opt = Flux.Optimise.Optimiser(AdaBelief(), WeightDecay(1f-4));
+	opt = AdaBelief()
+	ps = Flux.params(model)
+	all_solutions = []
+	losses = []
+	fvals = fill(typemax(Float64), length(solutions))
+	start_time = time()
+	logger=tblogger(filename*"_events.pb")
+	for epoch in 1:max_epochs
+		solved_before = issolved.(solutions)
+		offset = 1
+		while offset < length(solutions)
+			log_value(logger, "offset", offset)
+			offset, updated = update_solutions!(solutions, pddld, model, problem_files, dedup_fmb ∘ fminibatch, planner; offset, solve_solved, stop_after, max_time, artificial_goals)
+			# print some Statistics
+			solved = findall(issolved.(solutions))
+			print("offset = ", offset," updated = ", length(updated), " ")
+			log_value(logger, "solved", length(solved))
+			show_stats(solutions)
+			length(solved) == length(solutions) && break
+			
+			ii = [s !== nothing for s in solutions]
+			t₁ = @elapsed fvals[ii] .= train!(NeuroPlanner.loss, model, ps, opt, solutions[ii], fvals[ii], max_steps; max_loss, ϵ = epsilon, opt_type = Symbol(opt_type))
+			l = filter(x -> x != typemax(Float64), fvals)
+			log_value(logger, "mean error", mean(l))
+			log_value(logger, "worst case", maximum(l))
+			println("epoch = $(epoch) offset $(offset)  mean error = ", mean(l), " worst case = ", maximum(l)," time: = ", t₁)
+			time() - start_time > max_sim_time && break
+		end
+		solved_after = issolved.(solutions)
+		log_value(logger, "solved after epoch", sum(solved_after))
+		push!(losses, deepcopy(fvals))
+		push!(all_solutions, map(s -> issolved(s) ? s.stats : nothing , solutions))
+		if !isempty(filename)
+			serialize(filename*"_stats.jls", (;losses, all_solutions, model))
+		end
+		all(issolved.(solutions)) && break
+		if sum(solved_after) == sum(solved_before)  && double_maxtime
+			max_time *= 2
+		end
 
-            ii = [s !== nothing for s in solutions]
-            t₁ = @elapsed fvals[ii] .= train!(NeuroPlanner.loss, model, ps, opt, solutions[ii], fvals[ii], max_steps; max_loss, ϵ=epsilon, opt_type=Symbol(opt_type))
-            l = filter(x -> x != typemax(Float64), fvals)
-            log_value(logger, "mean error", mean(l))
-            log_value(logger, "worst case", maximum(l))
-            println("epoch = $(epoch) offset $(offset)  mean error = ", mean(l), " worst case = ", maximum(l), " time: = ", t₁)
-            time() - start_time > max_sim_time && break
-        end
-        solved_after = issolved.(solutions)
-        log_value(logger, "solved after epoch", sum(solved_after))
-        push!(losses, deepcopy(fvals))
-        push!(all_solutions, map(s -> issolved(s) ? s.stats : nothing, solutions))
-        if !isempty(filename)
-            serialize(filename * "_stats.jls", (; losses, all_solutions, model))
-        end
-        all(issolved.(solutions)) && break
-        if sum(solved_after) == sum(solved_before) && double_maxtime
-            max_time *= 2
-        end
-
-        time() - start_time > max_sim_time && break
-    end
-    all_solutions
+		time() - start_time > max_sim_time && break
+	end
+	all_solutions
 end
 
 
@@ -111,23 +111,23 @@ arch_name = "hgnn"
 planner_name = "astar"
 """
 
-@main function main(domain_name, arch_name, loss_name; max_steps::Int=10_000, max_time::Int=30, graph_layers::Int=2,
-    dense_dim::Int=16, dense_layers::Int=2, residual::String="none", seed::Int=1, planner_name::String="astar",
-    opt_type::String="mean", epsilon::Float64=0.0, max_loss::Float64=0.0, sort_by_complexity::Bool=false,
-    artificial_goals::Bool=false, double_maxtime::Bool=false, solve_solved::Bool=false, stop_after::Int=32,
-    max_epochs::Int=10, max_sim_time::Int=Int(23.5 * 3600))
-    Random.seed!(seed)
-    settings = (; domain_name, arch_name, loss_name, planner_name, max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, seed)
-    archs = Dict("asnet" => ASNet, "pddl" => MixedLRNN, "lrnn" => LRNN, "hgnnlite" => HGNNLite, "hgnn" => HGNN, "levinasnet" => LevinASNet)
-    planners = Dict("astar" => AStarPlanner, "bfs" => BFSPlanner,)
-    planner = planners[planner_name]
-    residual = Symbol(residual)
-    domain_pddl, problem_files = getproblem(domain_name, false)
-    fminibatch = NeuroPlanner.minibatchconstructor(loss_name)
-    hnet = archs[arch_name]
-    hid = hash((max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, max_epochs, max_sim_time))
-    filename = joinpath("selflearning", domain_name, join([arch_name, loss_name, planner_name, hid, seed], "_"))
-    isdir(dirname(filename)) || mkpath(dirname(filename))
-    open(fio -> print(fio, JSON.json(settings)), filename * "_config.json", "w")
-    experiment(domain_pddl, hnet, problem_files, fminibatch, planner; max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, max_epochs, max_sim_time, filename)
+@main function main(domain_name, arch_name, loss_name; max_steps::Int = 10_000, max_time::Int = 30, graph_layers::Int = 2, 
+		dense_dim::Int = 16, dense_layers::Int = 2, residual::String = "none", seed::Int = 1, planner_name::String = "astar", 
+		opt_type::String = "mean", epsilon::Float64 = 0.0, max_loss::Float64 = 0.0, sort_by_complexity::Bool = false, 
+		artificial_goals::Bool = false, double_maxtime::Bool = false, solve_solved::Bool = false, stop_after::Int = 32,
+		max_epochs::Int = 10, max_sim_time::Int = Int(23.5*3600))
+	Random.seed!(seed)
+	settings = (;domain_name, arch_name, loss_name, planner_name, max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, seed)
+	archs = Dict("asnet" => ASNet, "pddl" => MixedLRNN, "lrnn" => LRNN, "hgnnlite" => HGNNLite, "hgnn" => HGNN, "levinasnet" => LevinASNet)
+	planners = Dict("astar" => AStarPlanner, "bfs" => BFSPlanner,)
+	planner = planners[planner_name]
+	residual = Symbol(residual)
+	domain_pddl, problem_files = getproblem(domain_name, false)
+	fminibatch = NeuroPlanner.minibatchconstructor(loss_name)
+	hnet = archs[arch_name]
+	hid = hash((max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, max_epochs, max_sim_time))
+	filename = joinpath("selflearning", domain_name, join([arch_name, loss_name, planner_name, hid, seed], "_"))
+	isdir(dirname(filename)) || mkpath(dirname(filename))
+	open(fio -> print(fio, JSON.json(settings)), filename*"_config.json","w")
+	experiment(domain_pddl, hnet, problem_files, fminibatch, planner; max_steps, max_time, graph_layers, dense_dim, dense_layers, residual, opt_type, epsilon, max_loss, sort_by_complexity, artificial_goals, double_maxtime, solve_solved, stop_after, max_epochs, max_sim_time, filename)
 end
